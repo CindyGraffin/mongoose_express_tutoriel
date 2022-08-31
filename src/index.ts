07276003a04b4b9bc0581637bf8f34496afb0c70
CindyGraffin <graffincindy@gmail.com>
2022-08-31 09:52:04 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+const PORT = process.env.SERVER_PORT || 3000
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`it works on port ${PORT}`);
+})
